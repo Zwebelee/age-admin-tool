a07e817038edf1f7c626d0028956edc87ca14e83
Zwebelee <37217352+Zwebelee@users.noreply.github.com>
2024-10-02 17:46:19 +0200

    ✏️ typo in languages

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,5 +1,5 @@
 export const LANGUAGES = [
     { label: "English", code: "en" },
-    { label: "Deutsch", "code": "de"},
+    { label: "Deutsch", code: "de"},
     { label: "Français", code: "fr" }
 ];
