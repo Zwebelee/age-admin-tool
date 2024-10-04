a175c40f18716d69a29e1bbe90807bdc42a5b83b
Zwebelee <andi.eugster.5@gmail.com>
2024-10-04 22:07:31 +0200

    :sparkles: argparser to run debug True/False

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,12 @@
+import argparse
 from app.app import create_app
+
+# Setup argument parser
+parser = argparse.ArgumentParser(description="Run the Flask app.")
+parser.add_argument('--debug', action='store_true', help="Run the app in debug mode.")
+args = parser.parse_args()
+
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=args.debug)
