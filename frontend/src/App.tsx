417f72c647f0ab12548fb626a07cc872e124122e
Daniel Blöchlinger <haven@hispeed.ch>
2024-10-24 18:07:44 +0200

    ✨Add change theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import {Suspense} from "react";
+import {Suspense, useState} from "react";
 import './App.css'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {OverviewScreen} from "./screens/OverviewScreen.tsx";
@@ -10,44 +10,66 @@ import {ComponentsScreen} from "./screens/ComponentsScreen.tsx";
 import {ExperimentalScreen} from "./screens/ExperimentalScreen.tsx";
 import {Header} from "./components/Header.tsx";
 import {Sidebar} from "./components/Sidebar.tsx";
-import {Alert, Grid2, LinearProgress} from "@mui/material";
+import {Alert, LinearProgress} from "@mui/material";
+import Grid from '@mui/material/Grid2';
+import {createTheme, ThemeProvider} from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import {RootStore, RootStoreProvider, useRootStore} from "./stores/root-store.ts";
 import {observer} from "mobx-react-lite";
 
 const AppObserver = observer(() => {
-        if (!useRootStore().init) {
-            return <LinearProgress/>;
-        }
-        return (
-            <Suspense fallback="loading">
-                <BrowserRouter>
-                    <Grid2 container spacing={0}>
-                        <Grid2 size={12}>
-                            <p>Header</p>
-                            <Header/>
-                        </Grid2>
-                        <Grid2 size="auto">
-                            <p>Sidebar</p>
-                            <Sidebar/>
-                        </Grid2>
-                        <Grid2 size="auto">
-                            <p>Main</p>
-                            <Routes>
-                                <Route path="/" element={<OverviewScreen/>}/>
-                                <Route path="/users" element={<UsersScreen/>}/>
-                                <Route path="/contents" element={<ContentsScreen/>}/>
-                                <Route path="/tasks" element={<TasksScreen/>}/>
-                                <Route path="/groups" element={<GroupsScreen/>}/>
-                                <Route path="/components" element={<ComponentsScreen/>}/>
-                                <Route path="/experimental" element={<ExperimentalScreen/>}/>
-                            </Routes>
-                        </Grid2>
-                    </Grid2>
-                </BrowserRouter>
-            </Suspense>
-        )
+
+    const [toggleTheme, setToggleTheme] = useState(false);
+    const toggleSwitch = () => {
+        setToggleTheme(!toggleTheme);
+    };
+    const theme = createTheme({
+        palette: {
+            mode: toggleTheme ? "light" : "dark",
+            primary: {
+                main: "#90caf9",
+            },
+            secondary: {
+                main: "#f48fb1",
+            },
+        },
+    });
+
+    if (!useRootStore().init) {
+        return <LinearProgress/>;
     }
-);
+    return (
+        <Suspense fallback="loading">
+            <BrowserRouter>
+                <ThemeProvider theme={theme}>
+                        <CssBaseline />
+                        <Grid container spacing={0}>
+                            <Grid size={12}>
+                                <p>Header</p>
+                                <Header toggleTheme={toggleTheme} onChange={toggleSwitch} />
+                            </Grid>
+                            <Grid size="auto">
+                                <p>Sidebar</p>
+                                <Sidebar/>
+                            </Grid>
+                            <Grid size="auto">
+                                <p>Main</p>
+                                <Routes>
+                                    <Route path="/" element={<OverviewScreen/>}/>
+                                    <Route path="/users" element={<UsersScreen/>}/>
+                                    <Route path="/contents" element={<ContentsScreen/>}/>
+                                    <Route path="/tasks" element={<TasksScreen/>}/>
+                                    <Route path="/groups" element={<GroupsScreen/>}/>
+                                    <Route path="/components" element={<ComponentsScreen/>}/>
+                                    <Route path="/experimental" element={<ExperimentalScreen/>}/>
+                                </Routes>
+                            </Grid>
+                        </Grid>
+                </ThemeProvider>
+            </BrowserRouter>
+        </Suspense>
+    )
+});
 
 function App({rootStore}: { rootStore: RootStore }) {
     if (rootStore) {
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,12 +1,15 @@
 import {LanguageSelector} from "./LanguageSelector.tsx";
 import {useTranslation} from "react-i18next";
+import {Switch} from "@mui/material";
 
-export const Header = () => {
+export const Header = (props: any) => {
     const {t} = useTranslation()
     return (
         <>
             <h1>{t("app-title")}</h1>
             <LanguageSelector/>
+            <br/>
+            <p>Dark <Switch checked={props.toggleTheme} onChange={props.onChange}/> Light</p>
         </>
     )
 }
