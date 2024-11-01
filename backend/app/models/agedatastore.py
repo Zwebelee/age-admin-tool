0c69fe3fb20e9c3d93db923323f974cd442daa0f
Zwebelee <andi.eugster.5@gmail.com>
2024-11-01 22:30:19 +0100

    ✨ age components models

---
+++ backend/app/models/agedatastore.py
@@ -0,0 +1,18 @@
+from ..db import db
+from sqlalchemy import UUID
+
+
+class Agedatastore(db.Model):
+    __tablename__ = 'agedatastore'
+
+    guid = db.Column(UUID, primary_key=True, nullable=False, unique=True)
+    name = db.Column(db.String(120), nullable=False)
+
+    def to_dict(self):
+        return {
+            "guid": self.guid,
+            "name": self.name
+        }
+
+    def __repr__(self):
+        return f'AGE Data Store {self.name}'
