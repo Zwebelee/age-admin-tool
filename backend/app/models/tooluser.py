18ce3790e7881f26cf1e6107ee1204f1dde03b19
Zwebelee <andi.eugster.5@gmail.com>
2024-11-02 15:43:48 +0100

    ✨ models: tool-user, tool-roles

--- backend/app/models/tooluser.py
+++ backend/app/models/tooluser.py
@@ -1,4 +1,5 @@
 from .tasktooluser import task_tooluser
+from .tooluserrole import tooluser_role
 from ..db import db
 
 
@@ -7,6 +8,8 @@ class Tooluser(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     tasks = db.relationship('Task', secondary=task_tooluser, backref='toolusers')
+    roles = db.relationship('Toolrole', secondary=tooluser_role, backref='toolusers')
+
 
     def to_dict(self):
         return {
