867216263affe64fd330b528ac1c4103fa4d7d6e
Daniel Blöchlinger <haven@hispeed.ch>
2024-11-06 20:57:57 +0100

    🐛 Fix error in Header.tsx

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -18,7 +18,7 @@ export const Header = ({ toggleTheme, toggleMenu, onClickMenu, onClickLogo }: { 
                 <img src={logo} alt="Logo Kanton Luzern" className="logo"/>
             </NavLink>
             <h1>{t("app-title")}</h1>
-            <SecondaryMenu position="secondaryMenuDesktop" onClick={() => {}}/>
+            <SecondaryMenu position="secondaryMenuDesktop" onClickMenuItem={() => {}}/>
             <MobileMenu toggleMenu={toggleMenu} onClickMenu={onClickMenu}/>
         </div>
     )
