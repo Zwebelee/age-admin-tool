680a108be070b64d2dde3c24d8d49ca6ca986672
Zwebelee <andi.eugster.5@gmail.com>
2024-11-18 16:18:12 +0100

    🚧 TestComp for StoreLoading

--- frontend/src/components/StoreTesting.tsx
+++ frontend/src/components/StoreTesting.tsx
@@ -3,8 +3,19 @@ import {useRootStore} from "../stores/root-store.ts";
 import {observer} from "mobx-react-lite";
 
 export const TestStoreComponent = observer(() => {
+   return (
+       <Box>
+           <Typography variant="h3">TestStoreComponent</Typography>
+           <TestAgeStore />
+           <TestLicenseStore/>
+       </Box>
+   );
+});
+
+export const TestAgeStore = observer(() => {
     const rootstore = useRootStore();
     const ageStore = rootstore.ageStore;
+
 
     if (ageStore.isLoading) {
         return <CircularProgress />;
@@ -13,10 +24,34 @@ export const TestStoreComponent = observer(() => {
 
     if (ageStore.isLoaded ){
         return (
-            <Box>
-                <Typography variant="h3">TestStoreComponent</Typography>
+            <Box sx={{border: '3px solid blue', margin: '2px', padding: '10px', borderRadius: '8px'}}>
+                <Typography variant="h5">AGE Store</Typography>
                 <Typography variant="body1">Age: {ageStore.age?.guid}</Typography>
                 <Typography variant="body1">Age: {ageStore.age?.alias}</Typography>
+            </Box>
+        )
+    }
+
+    return <Typography variant="body1">Error loading data</Typography>
+});
+
+export const TestLicenseStore = observer(() => {
+    const rootstore = useRootStore();
+    const licenseStore = rootstore.portalLicenseStore;
+
+    if (licenseStore.isLoading) {
+        return <CircularProgress />;
+    }
+
+    if (licenseStore.isLoaded) {
+        return (
+            <Box sx={{border: '3px solid blue', margin: '2px', padding: '10px', borderRadius: '8px'}}>
+                <Typography variant="h5">License Store</Typography>
+                {Array.from(licenseStore.items.values()).map((item) => {
+                    return (
+                        <Typography key={item.guid} variant="body1">License: {item.name}: {item.currentusers}/{item.maxusers}</Typography>
+                    )
+                })}
             </Box>
         )
     }
