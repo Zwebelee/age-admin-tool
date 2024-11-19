2a22873492c3c7956768ef97ab1a6ea523cf8f50
Daniel Blöchlinger <haven@hispeed.ch>
2024-11-19 19:06:22 +0100

    💄 Modify header styles

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -14,12 +14,18 @@ export const Header = ({ toggleTheme, toggleMenu, onClickMenu, onClickLogo }: { 
     const logo = toggleTheme ? logoLight : logoDark;
     return (
         <div className="header">
-            <NavLink to="/" onClick={onClickLogo}>
-                <img src={logo} alt="Logo Kanton Luzern" className="logo"/>
-            </NavLink>
-            <h1>{t("app-title")}</h1>
-            <SecondaryMenu position="secondaryMenuDesktop" onClickMenuItem={() => {}}/>
-            <MobileMenu toggleMenu={toggleMenu} onClickMenu={onClickMenu}/>
+            <div className="header__logo">
+                <NavLink to="/" onClick={onClickLogo} className="header__logoLink">
+                    <img src={logo} alt="Logo Kanton Luzern" className="header__logoImg"/>
+                </NavLink>
+            </div>
+            <div className="header__title">
+                <h1 className="header__name">{t("app-title")}</h1>
+            </div>
+            <div className="header__menu">
+                <SecondaryMenu position="secondaryMenuDesktop" onClickMenuItem={() => {}}/>
+                <MobileMenu toggleMenu={toggleMenu} onClickMenu={onClickMenu}/>
+            </div>
         </div>
     )
 }
