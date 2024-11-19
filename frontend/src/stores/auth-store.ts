977cb10ed47786ad350c12250b9e2b9f082d6490
Zwebelee <andi.eugster.5@gmail.com>
2024-11-19 19:48:33 +0100

    ✨ Auth-Cookie

--- frontend/src/stores/auth-store.ts
+++ frontend/src/stores/auth-store.ts
@@ -1,23 +1,46 @@
 import {makeAutoObservable} from "mobx";
 import axios from "axios";
 import {RootStore} from "./root-store.ts";
+import {getCookie, setCookie} from "../utils/cookie.ts";
+
+interface LoginCredentials {
+    username: string;
+    password: string;
+}
 
 export class AuthStore {
-    accessToken: string | null = null;
+    accessToken: string = "";
     refreshToken: string | null = null;
     isLoggedIn: boolean = false;
 
     constructor(private rootStore: RootStore) {
         makeAutoObservable(this);
+        this.initialize();
     }
 
-    async login(username: string, password: string) {
+    initialize() {
+        const token = getCookie('token');
+        if (token) {
+            console.log('debug: token found!') //TODO: Cleanup!
+            this.accessToken = token;
+            this.isLoggedIn = true;
+        }
+    }
+
+    async login(loginCredentials: LoginCredentials) {
         try {
-            const response = await axios.post('http://localhost:5001/login', {username, password});
+            const response = await axios.post('http://localhost:5001/login', loginCredentials);
             this.accessToken = response.data.access_token;
             this.refreshToken = response.data.refresh_token;
             this.isLoggedIn = true;
             console.log('Login successful');
+
+            setCookie('token', this.accessToken, {
+                expires: 15 *60,
+                secure: true,
+                sameSite: 'strict'
+            })
+
 
         } catch (error) {
             console.error("Login failed", error);
@@ -35,7 +58,7 @@ export class AuthStore {
     }
 
     logout() {
-        this.accessToken = null;
+        this.accessToken = "";
         this.refreshToken = null;
         this.isLoggedIn = false;
     }
