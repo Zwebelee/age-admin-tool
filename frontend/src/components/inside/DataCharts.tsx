8d158abc483ace70198d153dd2a427ce4f4b4b21
Daniel Blöchlinger <haven@hispeed.ch>
2024-12-04 15:00:57 +0100

    ⚗️ Add experimental chart

--- frontend/src/components/inside/DataCharts.tsx
+++ frontend/src/components/inside/DataCharts.tsx
@@ -1,4 +1,7 @@
+import { PieChart } from "@mui/x-charts/PieChart"; // Chart 1, Installed Package
+
 import "./DataCharts.scss";
+
 
 export const DataCharts = ({ color, display }: {
     color: string;
@@ -7,7 +10,26 @@ export const DataCharts = ({ color, display }: {
     const dynamicClass = display ? "dataCharts dataCharts" + color : "dataChartsHidden";
     return (
         <div className={dynamicClass}>
-            <p>Charts</p>
+
+            {/* Chart 1 */}
+            <PieChart
+                series={[
+                    {
+                        data: [
+                            { id: 0, value: 10, label: "series A" },
+                            { id: 1, value: 15, label: "series B" },
+                            { id: 2, value: 20, label: "series C" },
+                        ],
+                        innerRadius: 30,
+                        outerRadius: 100,
+                        highlightScope: { fade: "global", highlight: "item" },
+                        faded: { innerRadius: 30, additionalRadius: -30, color: "gray" },
+                    },
+                ]}
+                width={400}
+                height={200}
+            />
+
         </div>
     );
 };
