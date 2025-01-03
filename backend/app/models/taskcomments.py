405a277474c1e8a776649ce64247d402f51cb518
Zwebelee <37217352+Zwebelee@users.noreply.github.com>
2025-01-03 14:29:42 +0100

    🐛 TaskCommment-Dict

--- backend/app/models/taskcomments.py
+++ backend/app/models/taskcomments.py
@@ -8,3 +8,12 @@ class TaskComment(db.Model):
     comment = db.Column(db.Text, nullable=False)
     tooluser_id = db.Column(db.Integer, db.ForeignKey('toolusers.id'))
     created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "task_id": self.task_id,
+            "comment": self.comment,
+            "tooluser_id": self.tooluser_id,
+            "created_at": self.created_at
+        }
