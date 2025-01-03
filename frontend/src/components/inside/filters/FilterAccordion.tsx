c4bbeb0e527f25ad789ee651066b66b2c2505255
Daniel Blöchlinger <haven@hispeed.ch>
2025-01-03 18:13:12 +0100

    💄 Responsive inside page

--- frontend/src/components/inside/filters/FilterAccordion.tsx
+++ frontend/src/components/inside/filters/FilterAccordion.tsx
@@ -110,7 +110,6 @@ export const FilterAccordion = ({
         PaperProps: {
             style: {
                 maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-                width: "100%",
             },
         },
     };
