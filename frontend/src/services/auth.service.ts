406e1f4fb36377bd608157eb9aa646c1fcd76045
Zwebelee <37217352+Zwebelee@users.noreply.github.com>
2025-01-04 17:38:30 +0100

    ⚰️ uncommented code

--- frontend/src/services/auth.service.ts
+++ frontend/src/services/auth.service.ts
@@ -44,33 +44,3 @@ export class AuthService {
         return this.apiClient;
     }
 }
-//
-// // Add a request interceptor
-// apiClient.interceptors.request.use(
-//     async (config) => {
-//         if (authStore.accessToken) {
-//             config.headers["Authorization"] = `Bearer ${authStore.accessToken}`;
-//         }
-//         return config;
-//     },
-//     (error) => Promise.reject(error)
-// );
-//
-// // Add a response interceptor
-// apiClient.interceptors.response.use(
-//     (response) => response,
-//     async (error) => {
-//         if (error.response?.status === 401 && error.config && !error.config._retry) {
-//             // Attempt to refresh the token
-//             error.config._retry = true;
-//             await authStore.refreshAccessToken();
-//             if (authStore.accessToken) {
-//                 error.config.headers["Authorization"] = `Bearer ${authStore.accessToken}`;
-//                 return apiClient(error.config);
-//             }
-//         }
-//         return Promise.reject(error);
-//     }
-// );
-//
-// export default apiClient;
