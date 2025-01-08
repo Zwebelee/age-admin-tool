1d721e87ab65a77308c3a6ee1ecc7bf99e5a105a
Daniel Blöchlinger <haven@hispeed.ch>
2025-01-08 16:55:57 +0100

    💄 Style and translate no charts

--- frontend/src/components/inside/charts/NoCharts.tsx
+++ frontend/src/components/inside/charts/NoCharts.tsx
@@ -1,5 +1,14 @@
+import {useTranslation} from "react-i18next";
+import "./NoCharts.scss";
+
+
 export const NoCharts = () => {
+
+    const { t } = useTranslation();
+
     return (
-        <p>Keine Diagramme vorhanden</p>
+        <div className="noCharts">
+            <p>{t("no-charts")}</p>
+        </div>
     )
 }
