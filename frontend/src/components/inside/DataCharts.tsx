7af10b59e9f75a6a4142eb9a4bc406e9f2d82ed9
Daniel Blöchlinger <haven@hispeed.ch>
2025-01-08 16:23:57 +0100

    💄 Optimize chart style

--- frontend/src/components/inside/DataCharts.tsx
+++ frontend/src/components/inside/DataCharts.tsx
@@ -16,22 +16,25 @@ export const DataCharts = ({ color, display, filterView }: dataChartsProps) => {
         <div className={dynamicClass2}>
 
             {/* Chart 1 */}
+            <h3>Anzahl Lizenzen nach Typ</h3>
             <PieChart
                 series={[
                     {
                         data: [
-                            { id: 0, value: 10, label: "series A" },
-                            { id: 1, value: 15, label: "series B" },
-                            { id: 2, value: 20, label: "series C" },
+                            { id: 0, value: 10, label: "type1", color: "rgba(255, 241, 117, 1.00)" },
+                            { id: 1, value: 15, label: "type2", color: "rgba(205, 225, 139, 1.00)" },
+                            { id: 2, value: 20, label: "type3", color: "rgba(157, 209, 160, 1.00)" },
+                            { id: 3, value: 22, label: "type4", color: "rgba( 69, 181, 198, 1.00)" },
+                            { id: 4, value: 18, label: "type5", color: "rgba(  0, 159, 227, 1.00)" },
                         ],
                         innerRadius: 30,
-                        outerRadius: 100,
+                        outerRadius: 120,
                         highlightScope: { fade: "global", highlight: "item" },
                         faded: { innerRadius: 30, additionalRadius: -30, color: "gray" },
                     },
                 ]}
                 width={400}
-                height={200}
+                height={300}
             />
 
         </div>
