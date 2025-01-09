62388d6e19129f956a3c4f0a61a4cd31737c705d
Daniel Blöchlinger <haven@hispeed.ch>
2025-01-09 18:07:40 +0100

    🚧 Dialog on task table styled

--- frontend/src/components/tasks/TaskDetails.tsx
+++ frontend/src/components/tasks/TaskDetails.tsx
@@ -8,21 +8,33 @@ import {
     FormControlLabel, List, ListItem, ListItemText,
     MenuItem,
     Select,
-    Typography
+    Typography,
 } from "@mui/material";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
 import {useRootStore} from "../../stores/root-store.ts";
 import {useEffect, useState} from "react";
 import {ToolUser} from "../../models/tooluser.ts";
 import {TaskComment} from "../../models/taskcomment.ts";
-import { SelectChangeEvent } from "@mui/material";
+import {SelectChangeEvent} from "@mui/material";
+import {useTranslation} from "react-i18next";
+
+import "./TaskDetails.scss";
+
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
+
 
 interface TaskDetailsDialogProps {
     open: boolean;
     onClose: () => void;
 }
 
+
 export const TaskDetailsDialog = ({open, onClose}: TaskDetailsDialogProps) => {
 
+    const {t} = useTranslation();
     const {taskStore, toolUserStore, taskCommentStore} = useRootStore();
     const task = taskStore.selectedItem;
     const [assignedTo, setAssignedTo] = useState(task?.assignedTo || "");
@@ -56,70 +68,183 @@ export const TaskDetailsDialog = ({open, onClose}: TaskDetailsDialogProps) => {
     };
 
 
+
     return (
-        <Dialog open={open} onClose={onClose}>
-            <DialogTitle>Task Details</DialogTitle>
+        <Dialog
+            className="taskDetails"
+            open={open}
+            onClose={onClose}
+        >
+            <DialogTitle>{t("task-details")}</DialogTitle>
             <DialogContent>
                 {task ? (
                     <>
-                        <Typography>ID: {task.guid}</Typography>
-                        <Typography>Title: {task.title}</Typography>
-                        <Typography>Description: {task.description}</Typography>
-                        <Typography>Status: {task.status}</Typography>
-                        <Typography>Priority: {task.priority}</Typography>
-                        <Typography>Assigned To: {task.assignedTo}</Typography>
-                        <Typography>Created At: {"task.createdAt.toString()"}</Typography>
-                        <Typography>Updated At: {"task.updatedAt.toString()"}</Typography>
-                        <Typography>Linked Object GUID: {task.linkedObjectGuid}</Typography>
-                        <Typography>Linked Object Type: {task.linkedObjectType}</Typography>
-                        <Typography>Task Rule GUID: {task.taskRuleGuid}</Typography>
+                        <Accordion>
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel1-content"
+                                id="panel1-header"
+                            >
+                                <Typography component="span">{t("task-information")}</Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <List
+                                    className="taskDetails__list"
+                                    sx={{ listStyleType: "disc" }}>
+                                    <ListItem>
+                                        <ListItemText
+                                            primary="ID"
+                                            secondary={task.guid}
+                                            sx={{ display: "list-item" }}
+                                        />
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListItemText
+                                            primary={t("title")}
+                                            secondary={task.title}
+                                            sx={{ display: "list-item" }}
+                                        />
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListItemText
+                                            primary={t("description")}
+                                            secondary={task.description}
+                                            sx={{ display: "list-item" }}
+                                        />
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListItemText
+                                            primary={t("priority")}
+                                            secondary={task.priority}
+                                            sx={{ display: "list-item" }}
+                                        />
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListItemText
+                                            primary={t("assigned-to")}
+                                            secondary={task.assignedTo}
+                                            sx={{ display: "list-item" }}
+                                        />
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListItemText
+                                            primary={t("created")}
+                                            secondary={"task.createdAt.toString()"}
+                                            sx={{ display: "list-item" }}
+                                        />
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListItemText
+                                            primary={t("modified")}
+                                            secondary={"task.updatedAt.toString()"}
+                                            sx={{ display: "list-item" }}
+                                        />
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListItemText
+                                            primary={t("linked-object-guid")}
+                                            secondary={task.linkedObjectGuid}
+                                            sx={{ display: "list-item" }}
+                                        />
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListItemText
+                                            primary={t("linked-object-type")}
+                                            secondary={task.linkedObjectType}
+                                            sx={{ display: "list-item" }}
+                                        />
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListItemText
+                                            primary={t("task-rule-guid")}
+                                            secondary={task.taskRuleGuid}
+                                            sx={{ display: "list-item" }}
+                                        />
+                                    </ListItem>
+                                </List>
+                            </AccordionDetails>
+                        </Accordion>
 
-                        <form>
-                            <FormControl>
-                                <FormControlLabel
-                                    control={<Checkbox name="completed"/>}
-                                    label="Completed"
-                                />
-                            </FormControl>
-                            <FormControl fullWidth>
-                                <Select
-                                    value={assignedTo}
-                                    onChange={handleAssignedToChange}
-                                    displayEmpty
-                                    inputProps={{'aria-label': 'Assigned To'}}
+                        <Accordion>
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel2-content"
+                                id="panel2-header"
+                            >
+                                <Typography component="span">{t("task-edit")}</Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <form>
+                                    <FormControl>
+                                        <FormControlLabel
+                                            control={<Checkbox name="completed"/>}
+                                            label="Completed"
+                                        />
+                                    </FormControl>
+                                    <FormControl fullWidth>
+                                        <Select
+                                            value={assignedTo}
+                                            onChange={handleAssignedToChange}
+                                            displayEmpty
+                                            inputProps={{"aria-label": "Assigned To"}}
+                                        >
+                                            <MenuItem value="">
+                                                <em>None</em>
+                                            </MenuItem>
+                                            {toolUsers.map(user => (
+                                                <MenuItem key={user.guid} value={user.guid}>
+                                                    {user.username}
+                                                </MenuItem>
+                                            ))}
+                                        </Select>
+                                    </FormControl>
+                                </form>
+                            </AccordionDetails>
+                        </Accordion>
+
+                        <Accordion>
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel3-content"
+                                id="panel3-header"
+                            >
+                                <Typography component="span">{t("task-comments")}</Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <List
+                                    sx={{ listStyleType: "disc" }}
                                 >
-                                    <MenuItem value="">
-                                        <em>None</em>
-                                    </MenuItem>
-                                    {toolUsers.map(user => (
-                                        <MenuItem key={user.guid} value={user.guid}>
-                                            {user.username}
-                                        </MenuItem>
+                                    {taskComments.map(comment => (
+                                        <ListItem key={comment.guid}>
+                                            <ListItemText
+                                                primary={comment.comment}
+                                                secondary={`By ${toolUsers.find(user => user.guid === comment.tooluserGuid)?.username || "Unknown"} on ${new Date(comment.createdAt).toLocaleString()}`}
+                                                sx={{ display: "list-item" }}
+                                            />
+                                        </ListItem>
                                     ))}
-                                </Select>
-                            </FormControl>
-                        </form>
-                        <Typography variant="h6">Comments</Typography>
-                        <List>
-                            {taskComments.map(comment => (
-                                <ListItem key={comment.guid}>
-                                    <ListItemText
-                                        primary={comment.comment}
-                                        secondary={`By ${toolUsers.find(user => user.guid === comment.tooluserGuid)?.username || 'Unknown'} on ${new Date(comment.createdAt).toLocaleString()}`}
-                                    />
-                                </ListItem>
-                            ))}
-                        </List>
-                        <Button variant={"contained"}>add a new comment</Button>
-
+                                </List>
+                                <div className="taskDetails__commentButton">
+                                    <Button variant={"contained"}>
+                                        {t("new-comment")}
+                                    </Button>
+                                </div>
+                            </AccordionDetails>
+                        </Accordion>
 
                     </>
                 ) : (
                     <Typography>Invalid Task</Typography>
                 )}
             </DialogContent>
-            <DialogActions>
-                <Button onClick={onClose}>Close</Button>
+            <DialogActions
+                className="taskDetails__closeButton"
+            >
+                <Button
+                    size="small"
+                    endIcon={<CloseOutlinedIcon/>}
+                    onClick={onClose}
+                >{t("actions.close")}</Button>
             </DialogActions>
         </Dialog>
     );
